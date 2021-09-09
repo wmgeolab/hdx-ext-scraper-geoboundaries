c2fb516bc056640921e3dd44e6824e63cf863393
Mike <rans@un.org>
2021-09-10 11:43:34 +1200

    Run shed

--- geoboundaries.py
+++ geoboundaries.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 """
 CODS:
 -----
@@ -25,12 +24,17 @@ def get_data(downloader, url):
     admin_boundaries = dict()
     ignored_countries = set()
     for boundaryinfo in downloader.get_json():
-        if boundaryinfo['worldBankIncomeGroup'] in ('High-income Countries', 'No income group available'):
-            ignored_countries.add(boundaryinfo['boundaryName'])
+        if boundaryinfo["worldBankIncomeGroup"] in (
+            "High-income Countries",
+            "No income group available",
+        ):
+            ignored_countries.add(boundaryinfo["boundaryName"])
             continue
-        countryiso3 = boundaryinfo['boundaryISO']
+        countryiso3 = boundaryinfo["boundaryISO"]
         dict_of_lists_add(admin_boundaries, countryiso3, boundaryinfo)
-    logger.info(f'Ignoring high income/no income available countries: {", ".join(sorted(ignored_countries))}')
+    logger.info(
+        f'Ignoring high income/no income available countries: {", ".join(sorted(ignored_countries))}'
+    )
     return admin_boundaries
 
 
@@ -40,62 +44,67 @@ def get_name_url(url):
 
 def generate_dataset(countryiso3, admin_boundaries):
     countryname = Country.get_country_name_from_iso3(countryiso3)
-    title = f'{countryname} - Subnational Administrative Boundaries'
-    logger.info(f'Creating dataset: {title}')
-    name = f'geoBoundaries admin boundaries for {countryname}'
+    title = f"{countryname} - Subnational Administrative Boundaries"
+    logger.info(f"Creating dataset: {title}")
+    name = f"geoBoundaries admin boundaries for {countryname}"
     slugified_name = slugify(name).lower()
-    dataset = Dataset({
-        'name': slugified_name,
-        'title': title
-    })
+    dataset = Dataset({"name": slugified_name, "title": title})
     try:
         dataset.add_country_location(countryiso3)
     except HDXError as e:
-        logger.error(f'{title} has a problem! {e}')
+        logger.error(f"{title} has a problem! {e}")
         return None, None
-    dataset.set_maintainer('6e0ed1f7-11df-4072-b2df-1c52527faae8')
-    dataset.set_organization('8be95204-f453-4b66-a4f6-dbe84cb0bdee')
-    dataset.set_expected_update_frequency('Live')
+    dataset.set_maintainer("6e0ed1f7-11df-4072-b2df-1c52527faae8")
+    dataset.set_organization("8be95204-f453-4b66-a4f6-dbe84cb0bdee")
+    dataset.set_expected_update_frequency("Live")
     dataset.set_subnational(True)
-    dataset.add_tags(['administrative divisions', 'geodata', 'gazetteer'])
+    dataset.add_tags(["administrative divisions", "geodata", "gazetteer"])
 
     sources = set()
     dataset_years = set()
     resource_names = list()
 
-    def add_resource(key, description, filetype='geojson'):
+    def add_resource(key, description, filetype="geojson"):
         name, url = get_name_url(admin_boundary[key])
-        resource = Resource({
-            'name': name,
-            'url': url,
-            'description': description
-        })
+        resource = Resource({"name": name, "url": url, "description": description})
         resource.set_file_type(filetype)
         resource_names.append(name)
         dataset.add_update_resource(resource)
 
     all_hdx = True
-    for admin_boundary in sorted(admin_boundaries, key=lambda x: x['boundaryType']):
-        if 'data.humdata.org' not in admin_boundary['boundarySourceURL']:
+    for admin_boundary in sorted(admin_boundaries, key=lambda x: x["boundaryType"]):
+        if "data.humdata.org" not in admin_boundary["boundarySourceURL"]:
             all_hdx = False
-        dataset_years.add(admin_boundary['boundaryYearRepresented'].replace('.0', ''))
+        dataset_years.add(admin_boundary["boundaryYearRepresented"].replace(".0", ""))
         i = 1
         while True:
-            source = admin_boundary.get(f'boundarySource-{i}')
+            source = admin_boundary.get(f"boundarySource-{i}")
             if source is None:
                 break
             sources.add(source)
             i += 1
-        boundarytype = admin_boundary['boundaryType']
-        add_resource('simplifiedGeometryGeoJSON', f'Simplified GeoJSON {boundarytype} boundaries for {countryname}')
-        add_resource('gjDownloadURL', f'GeoJSON {boundarytype} boundaries for {countryname}')
-        add_resource('tjDownloadURL', f'TopoJSON {boundarytype} boundaries for {countryname}')
-        add_resource('downloadURL', f'Other formats including shape file {boundarytype} boundaries for {countryname}',
-                     'shp')
+        boundarytype = admin_boundary["boundaryType"]
+        add_resource(
+            "simplifiedGeometryGeoJSON",
+            f"Simplified GeoJSON {boundarytype} boundaries for {countryname}",
+        )
+        add_resource(
+            "gjDownloadURL", f"GeoJSON {boundarytype} boundaries for {countryname}"
+        )
+        add_resource(
+            "tjDownloadURL", f"TopoJSON {boundarytype} boundaries for {countryname}"
+        )
+        add_resource(
+            "downloadURL",
+            f"Other formats including shape file {boundarytype} boundaries for {countryname}",
+            "shp",
+        )
     if all_hdx:
-        logger.info(f'Ignoring {countryname} as data for all admin levels comes from HDX!')
+        logger.info(
+            f"Ignoring {countryname} as data for all admin levels comes from HDX!"
+        )
         return None, None
     dataset_years = sorted(dataset_years)
     dataset.set_dataset_year_range(dataset_years[0], dataset_years[-1])
-    dataset['dataset_source'] = ', '.join(sorted(sources))
+    dataset["dataset_source"] = ", ".join(sorted(sources))
     return dataset, resource_names
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 """
 Top level script. Calls other functions that generate datasets that this script then creates in HDX.
 
@@ -9,17 +8,16 @@ import logging
 from os import environ
 from os.path import join
 
+from hdx.facades.simple import facade
 from hdx.hdx_configuration import Configuration
 from hdx.utilities.downloader import Download
-
-from hdx.facades.simple import facade
 from hdx.utilities.path import progress_storing_tempdir
 
-from geoboundaries import get_data, generate_dataset
+from geoboundaries import generate_dataset, get_data
 
 logger = logging.getLogger(__name__)
 
-lookup = 'hdx-ext-scraper-geoboundaries'
+lookup = "hdx-ext-scraper-geoboundaries"
 
 
 def main():
@@ -27,34 +25,56 @@ def main():
 
     configuration = Configuration.read()
     with Download() as downloader:
-        admin_boundaries = get_data(downloader, configuration['url'])
-        countries = [{'iso3': x} for x in sorted(admin_boundaries)]
-        logger.info(f'Number of countries to upload: {len(countries)}')
-        for info, country in progress_storing_tempdir(lookup, countries, 'iso3'):
-            countryiso3 = country['iso3']
+        admin_boundaries = get_data(downloader, configuration["url"])
+        countries = [{"iso3": x} for x in sorted(admin_boundaries)]
+        logger.info(f"Number of countries to upload: {len(countries)}")
+        for info, country in progress_storing_tempdir(lookup, countries, "iso3"):
+            countryiso3 = country["iso3"]
             country_admin_boundaries = admin_boundaries[countryiso3]
-            dataset, resource_names = generate_dataset(countryiso3, country_admin_boundaries)
+            dataset, resource_names = generate_dataset(
+                countryiso3, country_admin_boundaries
+            )
             if dataset:
                 dataset.update_from_yaml()
-                dataset.create_in_hdx(remove_additional_resources=True, hxl_update=False,
-                                      updated_by_script='GeoBoundaries', batch=info['batch'])
-                existing_order = [x['name'] for x in dataset.get_resources()]
+                dataset.create_in_hdx(
+                    remove_additional_resources=True,
+                    hxl_update=False,
+                    updated_by_script="GeoBoundaries",
+                    batch=info["batch"],
+                )
+                existing_order = [x["name"] for x in dataset.get_resources()]
                 if existing_order != resource_names:
-                    sorted_resources = sorted(dataset.get_resources(), key=lambda x: resource_names.index(x['name']))
-                    dataset.reorder_resources([x['id'] for x in sorted_resources], hxl_update=False)
+                    sorted_resources = sorted(
+                        dataset.get_resources(),
+                        key=lambda x: resource_names.index(x["name"]),
+                    )
+                    dataset.reorder_resources(
+                        [x["id"] for x in sorted_resources], hxl_update=False
+                    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=lookup)
-    parser.add_argument('-hk', '--hdx_key', default=None, help='HDX api key')
-    parser.add_argument('-hs', '--hdx_site', default=None, help='HDX site to use')
-    parser.add_argument('-s', '--start', default='RESET', help='ISO3 to start. Defaults to starting from beginning.')
+    parser.add_argument("-hk", "--hdx_key", default=None, help="HDX api key")
+    parser.add_argument("-hs", "--hdx_site", default=None, help="HDX site to use")
+    parser.add_argument(
+        "-s",
+        "--start",
+        default="RESET",
+        help="ISO3 to start. Defaults to starting from beginning.",
+    )
     args = parser.parse_args()
     hdx_site = args.hdx_site
     if hdx_site is None:
-        hdx_site = 'stage'
+        hdx_site = "stage"
     start = args.start
-    if start != 'RESET':
-        start = f'iso3={start}'
-    environ['WHERETOSTART'] = start
-    facade(main, hdx_key=args.hdx_key, hdx_site=hdx_site, user_agent=lookup, project_config_yaml=join('config', 'project_configuration.yml'))
+    if start != "RESET":
+        start = f"iso3={start}"
+    environ["WHERETOSTART"] = start
+    facade(
+        main,
+        hdx_key=args.hdx_key,
+        hdx_site=hdx_site,
+        user_agent=lookup,
+        project_config_yaml=join("config", "project_configuration.yml"),
+    )
